5eab2055dae28df3598f88b46f69a6e3e5f454a0
Nitin <nitinsharmaop828@gmail.com>
2026-06-02 20:44:55 +0530

    closuress

--- closures.js
+++ closures.js
@@ -11,5 +11,12 @@ function sum(){
     return num1 + num2;
 }
 
+function sum2(){
+    return function(){
+        return num1 + num2; 
+        /* here a function returns a function where it can access variables from its outer function after if executes.
+    }
+}
+                        // any function that reatins variables from its outer scope is called closure.
 console.log(sum());
 console.dir(sum());  //dir helps us to not read the body but also displays us the function as an object
